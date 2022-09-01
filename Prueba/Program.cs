ee3e7e21685c39a7e6a02ca191a0c88f88aec7c6
Jhanina Alarcón <jhanina2001.alarcon@gmail.com>
2022-08-31 20:53:37 -0500

    Agregar archivos de proyecto.

---
+++ Prueba/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1 = 0;
+            int valor2 = 0;
+            int valor3 = 0;
+            int valor4 = 0;
+            int valor5 = 0;
+            int valor6 = 0;
+            int valor7 = 0;
+            int valor8 = 0; 
+
+            Console.WriteLine("Ingrese x1:");
+            valor1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese y2:");
+            valor2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese x2:");
+            valor3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese y2:");
+            valor4 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese x3:");
+            valor5 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese y3:");
+            valor6 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese x4:");
+            valor7 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese y4:");
+            valor8 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Datos correctamente ingresados: ");
+
+            Rectangulo rectangulo = new Rectangulo
+            {
+                x1 = valor1,
+                y1 = valor2,
+                x2 = valor3,
+                y2 = valor4,
+                x3 = valor5,
+                y3 = valor6,
+                x4 = valor7,
+                y4 = valor8,
+            };
+            Console.WriteLine("El valor del área es: ");
+            Console.WriteLine(rectangulo.s());
+            Console.WriteLine("El valor del perimétro es: ");
+            Console.WriteLine(rectangulo.p());
+
+            Console.Read();
+        }
+    }
+}
---
+++ Prueba/Rectangulo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba
+{
+    public class Rectangulo
+    {
+        public int x1 { get; set; }
+        public int y1 { get; set; }
+        public int x2 { get; set; }
+        public int y2 { get; set; }
+        public int x3 { get; set; }
+        public int y3 { get; set; }
+        public int x4 { get; set; }
+        public int y4 { get; set; }
+
+        public double s()
+        {   
+            double altura = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
+            double baseee = Math.Sqrt((Math.Pow(x2 - x3, 2) + Math.Pow(y2 - y3, 2)));
+            return altura + baseee;
+        }
+
+        public double p()
+        {
+            double alt = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
+            double bs = Math.Sqrt((Math.Pow(x2 - x3, 2) + Math.Pow(y1 - y3, 2)));
+            double sumtot = 2 * alt + 2 * bs;
+            return sumtot;
+        }
+    }
+
+}
